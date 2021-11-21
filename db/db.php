1b45fc91be1e5fdd79f3e31a4dd5ebc4f1e00f3d
Luis Claudio <luisxd2@mail.com>
2021-11-21 17:27:43 -0400

    env add

--- db/db.php
+++ db/db.php
@@ -1,10 +1,10 @@
 <?php 
 class Conexion{	  
     public static function Conectar() {        
-        define('servidor', 'dbconection.cqjgcxgqtekt.us-east-1.rds.amazonaws.com');
-        define('nome_bd', 'dash_conection');
-        define('usuario', 'admin');
-        define('password', 'ChamaTech');					        
+        define('servidor', '--');
+        define('nome_bd', '==');
+        define('usuario', '==');
+        define('password', '--');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nome_bd, usuario, password, $opciones);			
